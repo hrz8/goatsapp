1b151b3f342057a295fa9e9779a7b7d184133285
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2024-06-17 19:11:28 +0700

    feat: add server cmd

--- assets/assets.go
+++ assets/assets.go
@@ -2,7 +2,28 @@ package assets
 
 import (
 	"embed"
+	"io"
+	"net/http"
 )
 
 //go:embed "public/*"
-var PublicFiles embed.FS
+var publicFiles embed.FS
+
+func PublicFile(w http.ResponseWriter, r *http.Request) {
+	filePath := r.URL.Path[len("/assets/"):]
+	file, err := publicFiles.Open("public/" + filePath)
+	if err != nil {
+		w.Write([]byte("not found"))
+		return
+	}
+	defer file.Close()
+
+	ff, ok := file.(io.ReadSeeker)
+	if !ok {
+		w.Write([]byte("not found"))
+		return
+	}
+
+	fileInfo, _ := file.Stat()
+	http.ServeContent(w, r, filePath, fileInfo.ModTime(), ff)
+}
