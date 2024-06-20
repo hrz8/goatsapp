1e699cac1a612b2edb04e3da40cb7c6753e06941
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2024-06-20 16:06:17 +0700

    feat: add server templates

--- pkg/viewutil/viewutil.go
+++ pkg/viewutil/viewutil.go
@@ -2,14 +2,7 @@ package viewutil
 
 import (
 	"context"
-	"net/http"
-
-	"github.com/a-h/templ"
 )
-
-func RenderView(ctx context.Context, w http.ResponseWriter, component templ.Component) error {
-	return component.Render(ctx, w)
-}
 
 func IsHtmxRequest(ctx context.Context) bool {
 	htmxRequest := ctx.Value("hx-request")
