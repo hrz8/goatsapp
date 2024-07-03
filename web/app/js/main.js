fbb5dfa322333a8a94b30a7474ff73c25569f73b
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2024-07-03 11:38:12 +0700

    ref: project switcher using event

--- web/app/js/main.js
+++ web/app/js/main.js
@@ -6,20 +6,52 @@ import {initFlowbite} from 'flowbite';
 import {Signal} from 'signal-polyfill';
 
 import {CodeMirror} from './codemirror';
-import {htmxForm} from './htmx-form';
-import {initProjectSelector, onLoadProjectSelector} from './project-switch';
 
 window.Signal = Signal;
 window.initFlowbite = initFlowbite;
-window.htmxForm = htmxForm;
 window.CodeMirror = CodeMirror;
-window.onLoadProjectSelector = onLoadProjectSelector;
 
-document.body.addEventListener('htmx:load', function () {
-  initFlowbite();
-  initProjectSelector();
+function InitProjectForm() {
+  const form = document.querySelector('#add-project-form');
+  form.addEventListener('htmx:beforeOnLoad', function (e) {
+    if (e.detail.xhr.status === 422) {
+      e.detail.shouldSwap = true;
+      e.detail.isError = false;
+    }
+  });
+  form.addEventListener('htmx:beforeRequest', function (e) {
+    const btn = e.detail.elt.querySelector('button[type="submit"');
+    btn.classList.add('hidden');
+  });
+  form.addEventListener('htmx:afterRequest', function (e) {
+    const btn = e.detail.elt.querySelector('button[type="submit"');
+    btn.classList.remove('hidden');
+    btn.classList.add('block');
+
+    if (e.detail.xhr.status === 200 || e.detail.xhr.status === 422) {
+      initFlowbite(); // need to re-init since gonna doing getInstance()
+      const modal = FlowbiteInstances.getInstance('Modal', 'add-project-modal');
+
+      if (e.detail.xhr.status === 200) {
+        modal.hide();
+        e.detail.elt.reset();
+      }
+
+      setTimeout(() => location.reload(), 2000);
+    }
+  });
+}
+
+window.addEventListener('load', function () {
+  InitProjectForm();
 });
 
 document.body.addEventListener('htmx:pushedIntoHistory', function () {
-  initProjectSelector();
+  // because the form (Modal) removed and replaced every time url history pushed
+  // need to re-init the listener
+  InitProjectForm();
+});
+
+document.body.addEventListener('htmx:load', function () {
+  initFlowbite();
 });
--- web/app/js/project-switch.js
+++ web/app/js/project-switch.js
@@ -1,55 +1,10 @@
-import {initFlowbite} from 'flowbite';
-
-import {htmxForm} from './htmx-form';
-
-function clearCheckedRadioBtnProject() {
+document.addEventListener('project:switch', function (e) {
+  // uncheck all checked
   document.querySelectorAll('.select-project.checked').forEach((checkedEl) => {
     checkedEl.classList.remove('checked');
   });
-}
-
-function switchProject(radio) {
-  const form = radio.closest('form');
+  // do htmx request to set project_id cookie
+  const form = e.detail.elt.closest('form');
   const submit = form.querySelector('button[type=submit]');
   submit.click();
-}
-
-function onLoadProjectSelector(dropdown) {
-  document.querySelectorAll(dropdown + ' .select-project').forEach((el) => {
-    el.addEventListener('click', function () {
-      clearCheckedRadioBtnProject();
-      switchProject(this);
-    });
-  });
-}
-
-function initProjectSelector() {
-  htmxForm(document.querySelector('#add-project-form'), {
-    before(e) {
-      const btn = e.detail.elt.querySelector('button[type="submit"');
-      btn.classList.add('hidden');
-    },
-    after(e) {
-      initFlowbite();
-      const btn = e.detail.elt.querySelector('button[type="submit"');
-      const toast = FlowbiteInstances.getInstance('Dismiss', 'toast');
-      const modal = FlowbiteInstances.getInstance('Modal', 'add-project-modal');
-      btn.classList.remove('hidden');
-      btn.classList.add('block');
-
-      if (e.detail.xhr.status === 200 || e.detail.xhr.status === 422) {
-        if (e.detail.xhr.status === 200) {
-          modal.hide();
-          e.detail.elt.reset();
-        }
-
-        setTimeout(() => {
-          toast.hide();
-          location.reload();
-        }, 2000);
-      }
-    },
-  });
-}
-
-export {initProjectSelector, onLoadProjectSelector};
+});
